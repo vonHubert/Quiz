8b7f1c2f6bb928095ac1a3e74a483b068042c0db
Sergey Sokolov <sergey.and.sokolov@gmail.com>
2022-11-12 23:42:53 +0400

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by MacBook Air 13 on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
